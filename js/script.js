e8bdb2005a9ba01c122a6cb15b56e694ebb279d6
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-05-20 09:50:49 +0530

    Commit 6: Show details added

--- js/script.js
+++ js/script.js
@@ -152,7 +152,7 @@ async function displayPopularShows() {
         />`;
 
     div.innerHTML = `
-      <a href="show-details.html?id=${show.id}">
+      <a href="tv-details.html?id=${show.id}">
         ${showPoster}
       </a>
       <div class="card-body">
@@ -165,6 +165,67 @@ async function displayPopularShows() {
 
     document.querySelector('#popular-shows').appendChild(div);
   });
+}
+
+// Display show details on the tv details page
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+  const show = await fetchAPIdata(`tv/${showId}`);
+  displayBackgroundImage('tv', show.backdrop_path); // Set background (overlay) image
+  const div = document.createElement('div');
+  const showPoster = show.poster_path
+    ? `<img
+      src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+    : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${show.name}"
+    />`;
+  div.innerHTML = `
+  <div class="details-top">
+          <div>
+            ${showPoster}
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Air Date: ${show.first_air_date}</p>
+            <p>${show.overview}</p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href=${
+              show.homepage
+            } target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${
+              show.number_of_episodes
+            }</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${
+                show.last_episode_to_air.name
+              }
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies
+            .map((company) => `${company.name}`)
+            .join(', ')}</div>
+        </div>
+  `;
+  document.querySelector('#show-details').appendChild(div);
 }
 
 // Highlight the active link in the nav bar
@@ -229,6 +290,7 @@ function init() {
 
     case '/tv-details.html':
       console.log('Welcome to the TV details page!');
+      displayShowDetails();
       break;
 
     case '/search.html':
