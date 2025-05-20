a36449a60e90d7d2d1c51d6aea3e7d5894258a84
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-05-20 08:44:07 +0530

    Commit 4: Movies details added

--- js/script.js
+++ js/script.js
@@ -66,12 +66,75 @@ async function displayPopularMovies() {
   });
 }
 
+// Display movie details on the movie details page
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIdata(`movie/${movieId}`);
+  const div = document.createElement('div');
+  const moviePoster = movie.poster_path
+    ? `<img
+      src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`
+    : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`;
+  div.innerHTML = `
+  <div class="details-top">
+          <div>
+      ${moviePoster}
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>${movie.overview}</p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+              movie.budget
+            )}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+              movie.revenue
+            )}</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${
+              movie.status
+            } </li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies
+            .map((company) => `${company.name}`)
+            .join(' ')}</div>
+        </div>
+  `;
+  document.querySelector('#movie-details').appendChild(div);
+}
+
 // Display popular TV shows on the shows page
 async function displayPopularShows() {
   const { results } = await fetchAPIdata('tv/popular');
 
   results.forEach((show) => {
-    console.log(show)
+    console.log(show);
     const div = document.createElement('div');
     div.classList.add('card');
 
@@ -113,6 +176,12 @@ function highLightActiveLink() {
   });
 }
 
+// Format numbers with commas
+// Example: 1000 -> 1,000
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // Routing
 function init() {
   switch (global.currentPage) {
@@ -124,11 +193,12 @@ function init() {
 
     case '/shows.html':
       console.log('Welcome to the shows page!');
-      displayPopularShows()
+      displayPopularShows();
       break;
 
     case '/movie-details.html':
       console.log('Welcome to the movie details page!');
+      displayMovieDetails();
       break;
 
     case '/tv-details.html':
